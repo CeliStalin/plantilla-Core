1037e92b373af428c94d0a545956651fdcf808b2
Stalin <stalin.celi@consalud.cl>
2025-05-06 14:06:57 -0400

    Mejoras sin errores

---
+++ babel.d.ts
@@ -0,0 +1,5 @@
+declare module '@babel/core';
+declare module '@babel/preset-env';
+declare module '@babel/preset-react';
+declare module '@babel/preset-typescript';
+declare module '@babel/plugin-transform-runtime';
